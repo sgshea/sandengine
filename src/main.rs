1320917998fde692924d3072177831a60ea3688f
sgshea <sammys512@yahoo.com>
2024-03-07 22:25:22 -0500

    feat: Inital implementation of cell swapping with density - currently panics can happen due to buggy get_cell code - should refactor a lot of the chunk/world interfacing - idea: implement density as a percentage chance to move (to simulate pixels falling slower through water)

--- src/main.rs
+++ src/main.rs
@@ -109,10 +109,12 @@ fn cell_at_pos_dbg(
     mut dbg_info: ResMut<DebugInfo>,
 ) {
     for sim in sim.iter() {
+        // round pos down
+        let pos = Vec2::new(pos.x.floor(), pos.y.floor());
         dbg_info.position = pos;
         dbg_info.chunk_position = Vec2::new((pos.x / CHUNK_SIZE.0 as f32).floor(), (pos.y / CHUNK_SIZE.1 as f32).floor());
         dbg_info.cell_position_in_chunk = Vec2::new((pos.x % CHUNK_SIZE.0 as f32).floor(), (pos.y % CHUNK_SIZE.1 as f32).floor());
-        dbg_info.hovered_cell = Some(sim.world.get_cell(pos.x as i32, pos.y as i32));
+        dbg_info.hovered_cell = Some(sim.world.get_cell(pos.x as i32, pos.y as i32).expect("Cell out of bounds"));
     }
 }
 
@@ -232,7 +234,7 @@ fn render_pixel_simulation(
         image.data.par_chunks_mut(4).enumerate().for_each(|(i, pixel)| {
             let x = i as i32 % WORLD_SIZE.0;
             let y = i as i32 / WORLD_SIZE.0;
-            let cell = sim.world.get_cell(x, y);
+            let cell = sim.world.get_cell(x, y).expect("Cell out of bounds");
             let color = cell.get_color();
             pixel.copy_from_slice(color);
         });
